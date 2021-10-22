d830f4071719a59872cd5800ab1ff9e2c06474cd
olin74 <olin@bk.ru>
2021-10-22 20:27:15 +0300

    app added

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 
 import telebot
 
-bot = telebot.TeleBot('2083207800:AAFZ1QgWt4mYRv2Aw3gI-i2fmjvvDjZoqH4')
+def app():
+    bot = telebot.TeleBot('2083207800:AAFZ1QgWt4mYRv2Aw3gI-i2fmjvvDjZoqH4')
 
+    @bot.message_handler(commands=['start'])
+    def start_message(message):
+        bot.send_message(message.chat.id, 'Бот в разработке, за Вами никто не приедет.')
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Бот в разработке, за Вами никто не приедет.')
+    @bot.message_handler(content_types=['text'])
+    def send_text(message):
+        bot.send_message(message.chat.id, 'Говорю же, никто не приедет. Ждите запуска бота')
 
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    bot.send_message(message.chat.id, 'Говорю же, никто не приедет. Ждите запуска бота')
+    bot.polling()
+
