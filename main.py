0050c208654c7d2e1ea7a4a027a2b77d82559d9d
olin74 <olin@bk.ru>
2021-10-23 14:31:47 +0300

    redis test

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-#pip install pytelegrambotapi
 
+import redis
 import telebot
 
 def app():
     bot = telebot.TeleBot('2083207800:AAFZ1QgWt4mYRv2Aw3gI-i2fmjvvDjZoqH4')
 
+    redis_data = redis.StrictRedis.from_url('redis://Z3rXtadcdy2nW0TIHXCCFP3J@ckv2rp80d000v0ub978gb171y:6379')
+    cm = redis_data.get('count_start')
+
+
     @bot.message_handler(commands=['start'])
     def start_message(message):
-        bot.send_message(message.chat.id, 'Бот в разработке, за Вами никто не приедет.')
+        redis_data['count_start'] += 1
+        bot.send_message(message.chat.id, f"Бот в разработке, за Вами никто не приедет. Запусков: {redis_data['count_start']}", )
 
     @bot.message_handler(content_types=['text'])
     def send_text(message):
