f738a03d5be94451767c5ed8c49328cb34fe58cc
olin74 <olin@bk.ru>
2021-11-28 09:46:47 +0300

    linmit 300

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ TELE_TOKEN = os.environ['TELEGRAM_TOKEN']
 SYMBOL = "₽"  # Символ валюты текущей денежной системы
 PLANET_RADIUS = 6371  # Радиус текущей планеты в км, погрешность 0.5%
 ADMIN_LIST = [665812965]  # Список админов для спец команд (тут только WhiteJoe)
-ABOUT_LIMIT = 100  # Лимит символов в объявлении
+ABOUT_LIMIT = 300  # Лимит символов в объявлении
 CONTENT_TYPES = ["text", "audio", "document", "photo", "sticker", "video", "video_note", "voice", "location", "contact",
                  "new_chat_members", "left_chat_member", "new_chat_title", "new_chat_photo", "delete_chat_photo",
                  "group_chat_created", "supergroup_chat_created", "channel_chat_created", "migrate_to_chat_id",
